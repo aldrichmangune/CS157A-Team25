d258839f316f9ae12eced5bec8defffc5043c02e
aldrichmangune <32374298+aldrichmangune@users.noreply.github.com>
2019-10-09 15:33:44 -0700

    Started implementation for templates

--- test-django/mysite/db/views.py
+++ test-django/mysite/db/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.template.response import TemplateResponse
+from django.utils import timezone
+
 from db.models import Textbook
+
 
 def index(request):
     t = Textbook.objects.all()
@@ -9,3 +14,13 @@ def index(request):
         html = html + text.textbook_text + "<br>"
     html = html + "</body></html>"
     return HttpResponse(html)
+
+def createTextbook(request):
+    if request.method == 'POST':
+        if request.POST.get('title'):
+            Textbook(textbook_text=request.POST.get('title'), pub_date=timezone.now()).save()
+            return HttpResponse("Successfully posted " + request.POST.get('title'))
+        else:
+            return HttpResponse("Something went wrong")
+    else:
+        return TemplateResponse(request, 'db/create_listing.html')
