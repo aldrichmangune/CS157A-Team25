9fffde18e8fe2b4c04dc1716db5bf43910d240ae
Au Tran <xtranx2@yahoo.com>
2019-10-28 21:36:04 -0700

    Deleted test Django project

--- test-django/mysite/db/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Textbook(models.Model):
-    textbook_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.textbook_text
